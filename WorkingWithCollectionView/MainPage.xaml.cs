33b43c8b5b792dda2580a69e71cc6fc276786c91
kittiSama <52246153+kittiSama@users.noreply.github.com>
2023-03-09 21:42:00 +0200

    Add files via upload

---
+++ WorkingWithCollectionView/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using BindingToObject.Models;
+using System.Collections.ObjectModel;
+
+namespace WorkingWithCollectionView;
+
+public partial class MainPage : ContentPage
+{
+    //אוסף התלמידים
+    public ObservableCollection<Student> Students { get; set; }
+
+    #region כדי שהמסך יתעדכן כתוצאה מעדכון נתון עלינו להפעיל את אירוע בכל שינוי ערך שלו. לכן עלינו ליצור שדות מגבה
+   
+    private Student _student;//תלמיד נבחר
+    #endregion
+
+    #region בכל שינוי בשדות נפעיל את האירוע
+    public Student Student { get => _student; set { if (_student != value) { _student = value; OnPropertyChanged("Student"); } } }
+    
+    #endregion
+
+    public MainPage()
+    {
+        //נגדיר רשימה ריקה
+        Students = new ObservableCollection<Student>();
+        //נאתחל את התלמיד הבודד לריק
+        Student = new() { Image="dotnet_bot.svg", Name="ברירת מחדל",BirthDate=new DateTime()};
+       
+        //נקשר את הדף שלנו לאובייקט המכיל את הקוד שלו
+        this.BindingContext = this;       
+        InitializeComponent();
+       
+	}
+
+    /// <summary>
+    /// פעולה הטוענת את נתוני התלמידים 
+    /// מכיוון ש
+    /// Students הוא ObeservableCollection
+    /// הפקד המקושר יתעדכן אוטומטית
+    /// </summary>
+    private void LoadStudents()
+    {
+        this.Students.Clear();
+       //דוגמה להוספת תלמיד בעקבות השינוי של גרסא 6 - אין צורך לציין אחרי
+       //new שם מחלקה
+        Students.Add(new() { Name = "Roni", Image = "roni.jpg", BirthDate = new DateTime(2006, 2, 19) });
+        //הוספת תלמיד בדרך המלאה
+        Students.Add(new Student { Name = "Omer", BirthDate = new DateTime(2006, 2, 9), Image = "omer.jpg" });
+        Students.Add(new() { Name = "Ofir", Image = "jofir.jpg", BirthDate = new DateTime(1800, 11, 12) });
+        Students.Add(new() { Name = "Aviv", Image = "aviv.jpg", BirthDate = new DateTime(2020, 1, 2) });
+        Students.Add(new() { Name = "Gil", Image = "gil.jpg", BirthDate = new DateTime(1984, 6, 10) });
+        Students.Add(new() { Name = "Maya", Image = "maya.jpg", BirthDate = new DateTime(2006, 10, 13) });
+
+        Student = Students[0];
+
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        LoadStudents();
+        OnPropertyChanged(nameof(Students));
+
+    }
+
+    private void SwipeItem_Clicked(object sender, EventArgs e)
+    {
+        Students.Remove(Student);
+    }
+    private void SwipeItem_ClickedCopy(object sender, EventArgs e)
+    {
+        Students.Add(Student);
+    }
+
+}
+
